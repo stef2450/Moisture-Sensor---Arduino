6350811123ad95450c4126f1284b89fcce701428
stef2450 <stefan.lemser@gmail.com>
2017-03-01 10:58:53 +0100

    Add files via upload

---
+++ Moisture.ino
@@ -0,0 +1,27 @@
+int sensorPin = A0;
+int sensorValue = 0;
+
+void setup() {
+
+  //Serial.begin(9600); //  (Saving for datalogging)
+}
+void loop() {
+              
+  //Serial.print("sensor = " );  //  (Saving for datalogging)
+  sensorValue = analogRead(sensorPin); 
+  //Serial.println(sensorValue); // (Saving for datalogging)
+
+  if(sensorValue >600){
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(100);                      
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(100);
+  }
+  if(sensorValue <600){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(sensorValue);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(sensorValue);
+  }
+}
+
