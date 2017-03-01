ca8d971c523850cc70371d67651da36a3a888393
stef2450 <stefan.lemser@gmail.com>
2017-03-01 12:43:28 +0100

    Add files via upload

--- Moisture.ino
+++ Moisture.ino
@@ -1,27 +1,29 @@
 int sensorPin = A0;
 int sensorValue = 0;
+int maxthreshold = 730;
+int minthreshold = 600;
 
 void setup() {
 
-  //Serial.begin(9600); //  (Saving for datalogging)
+  Serial.begin(9600); //  (Saving for datalogging)
 }
 void loop() {
               
-  //Serial.print("sensor = " );  //  (Saving for datalogging)
+  Serial.print("sensor = " );  //  (Saving for datalogging)
   sensorValue = analogRead(sensorPin); 
-  //Serial.println(sensorValue); // (Saving for datalogging)
+  Serial.println(sensorValue); // (Saving for datalogging)
 
-  if(sensorValue >600){
+  if(sensorValue >maxthreshold){
     digitalWrite(LED_BUILTIN, HIGH);   
-    delay(100);                      
+    delay(25);                      
     digitalWrite(LED_BUILTIN, LOW);  
-    delay(100);
-  }
-  if(sensorValue <600){
+    delay(25);
+  } else if(sensorValue <minthreshold){
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(sensorValue);
+    delay(500);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(sensorValue);
+    delay(500);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);    
   }
 }
-
